acf61f274c5dac85448f5eb1f505f907db37d56f
00-01 <59557934+00-01@users.noreply.github.com>
2022-02-09 10:32:19 +0900

    Update conver_h5_to tflite.py

--- conver_h5_to tflite.py
+++ conver_h5_to tflite.py
@@ -7,11 +7,13 @@ tflite_model = converter.convert()
 with open('model.tflite', 'wb') as f:
   f.write(tflite_model)
 
-
-
+# 1
 # converter = tf.lite.TFLiteConverter.from_keras_model(model)    # from_keras_model(model = tf.keras.models.Sequential())
+
+# 2
 # converter = tf.lite.TFLiteConverter.from_saved_model('/home/a/model/')    # from_saved_model_path
 
+# 3
 # class Squared(tf.Module):    # Create a model using low-level tf.* APIs
 #   @tf.function(input_signature=[tf.TensorSpec(shape=[None], dtype=tf.float32)])
 #   def __call__(self, x):
@@ -21,8 +23,6 @@ with open('model.tflite', 'wb') as f:
 # # (to generate a SavedModel) tf.saved_model.save(model, "saved_model_tf_dir")
 # concrete_func = model.__call__.get_concrete_function()
 # converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func], model)    # if TensorFlow 2.7 <, pass only first argument: from_concrete_functions([concrete_func])
-
-  
 
   
 ### ============================== COMMAND LINE VERSION ==============================
